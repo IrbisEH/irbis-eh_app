e10bdbc5bddb35f310338f6c314bb78af6e3d724
ehristenko <>
2023-05-02 21:35:19 +0300

    add ideas for navigation  menu

--- irbis-eh_app/static/js/git_popup.js
+++ irbis-eh_app/static/js/git_popup.js
@@ -1,3 +1,24 @@
 'use strict';
 import Noise from './effects/Noise.js';
 
+console.log("w");
+
+const socialLink = document.querySelector('.social-link');
+const shareLink = document.querySelector('.share-link');
+
+socialLink.addEventListener('click', function(){
+socialLink.classList.toggle('active');
+shareLink.classList.toggle('active');
+})
+
+// window.addEventListener('load', () => {
+//
+//     const socialLink = document.querySelector('.social-link');
+//     const shareLink = document.querySelector('.share-link');
+//
+//     socialLink.addEventListener('click', function(){
+//     socialLink.classList.toggle('active');
+//     shareLink.classList.toggle('active');
+//     })
+//
+// })
